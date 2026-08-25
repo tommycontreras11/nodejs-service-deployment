1749e54c01dbf30d7f9786733fc545adcc6f856e
Tommy Grullon <tommycontreras34@gmail.com>
2026-08-25 10:45:02 -0400

    feat: initialize Node.js service with Express framework
    - Created package.json for project configuration and dependencies
    - Implemented basic Express server in index.js
    - Set up a simple route to respond with "Hello, World!" on the root path

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const PORT = 3000
+
+const app = express()
+
+app.get("/", (_req, res) => {
+  res.status(200).send("Hello, World!")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
