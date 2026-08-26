c01030a85d2efdce90105dcc08e3c0913e04e607
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-08-25 21:40:17 -0400

    fix: update greeting message in root response

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,7 @@ const PORT = 3000
 const app = express()
 
 app.get("/", (_req, res) => {
-  res.status(200).send("Hello from CI/CD!")
+  res.status(200).send("Hello from CI/CD 2!")
 })
 
 app.listen(PORT, () => {
